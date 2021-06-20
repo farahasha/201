1e056f6ced8fc095689dbba4808798b6ebc3eabc
farahasha <farahasha93@yahoo.com>
2021-06-20 22:00:59 +0300

    add

---
+++ app.js
@@ -0,0 +1,11 @@
+let username = prompt('welcome,we can know your name?');
+alert("hello", username)
+console.log(username);
+
+let age = prompt('How old are you ?');
+console.log(age);
+let Family = prompt('How many members of your family?');
+console.log(Family);
+
+let Ameen = prompt('MAY ALLAH SAVE YOU')
+console.log(Ameen);
